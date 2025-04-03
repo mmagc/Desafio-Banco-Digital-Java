5f4e59c6cc6ba71562d7fc6e5c083faffc1edaf5
mmagc <119635234+mmagc@users.noreply.github.com>
2025-04-03 17:22:01 -0300

    Adição de criação de banco

--- src/desafioBanco/Banco.java
+++ src/desafioBanco/Banco.java
@@ -1,5 +1,6 @@
 package desafioBanco;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Banco {
@@ -7,11 +8,30 @@ public class Banco {
     public String nome;
     private List<Conta> contas;
 
+
+    public Banco(){
+
+        this.contas = new ArrayList<>();
+
+    }
+
+    public void adicionaConta(Conta conta){
+        this.contas.add(conta);
+    }
+
     public String getNome() {
         return nome;
     }
 
     public void setNome(String nome) {
         this.nome = nome;
+    }
+
+    @Override
+    public String toString() {
+        return "Banco{" +
+                "nome='" + nome + '\'' +
+                ", contas=" + contas +
+                '}';
     }
 }
--- src/desafioBanco/Main.java
+++ src/desafioBanco/Main.java
@@ -4,19 +4,23 @@ public class Main {
 
     public static void main(String[] args) {
 
+        Banco banco = new Banco();
+        banco.nome = "Caixa";
+
         Cliente matheus = new Cliente();
         matheus.nome = "Matheus";
 
         Conta cc = new contaCorrente(matheus);
         Conta poupanca = new contaPoupanca(matheus);
+        banco.adicionaConta(poupanca);
+        banco.adicionaConta(cc);
+        System.out.println(banco);
 
         cc.depositar(100);
         cc.transferir(50, poupanca);
 
         cc.imprimirExtrato();
         poupanca.imprimirExtrato();
-
-
 
     }
 
